ab3d35be3681a7e7a6ec82c9a9f20844f2292d5d
maicon2022 <maicon.felipe@academico.ifpb.edu.br>
2023-11-19 14:30:32 -0300

    Prática Hello world Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Usuario } from '../shared/model/usuario';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'pweb-angular';
+  
+  usuario!: Usuario; //está fazendo o two-way-data-binding -> está linkado, do html está caindo diretamente aqui
+  usuarios!: Array<Usuario>; //dizer o tipo do array <>, aqui eu apenas defini, mas eu tenho que fazer um objeto em memória a partir do new
+  //como uma classe posso criar um construtor
+  constructor() {
+    this.usuario = new Usuario(); //construindo um novo usuario, na memória ram existe esse objeto usuario
+    this.usuarios = new Array<Usuario>(); //posso inserir pois existe objeto em memória o qual posso trabalhar
+  }
+
+  insertUser(): void{ //espera um tipo de retorno, mas não vai retornar nada
+    this.usuarios.push(this.usuario); //array -> estou inserindo o mesmo objeto sempre, apontando para o mesmo objeto em memória Ram, vários ponteiros apontando para o mesmo canto
+    this.usuario = new Usuario(); //zero o objeto usuario, começando de novo, limpo e crio novo objeto em memória Ram numa nova posição
+  }
+
 }
---
+++ src/shared/model/usuario.ts
@@ -0,0 +1,8 @@
+//criação da classe
+export class Usuario {
+    //posso relaxar com as regras e métodos gets e sets
+    //atributos usados diretamente
+    name!: string;
+    cpf!: string;
+    age!: number;
+}
